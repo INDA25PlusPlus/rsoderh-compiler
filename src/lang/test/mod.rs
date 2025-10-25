e60327d225f3454ded31f9086887d5b7834c1095
laxsjo <rasmus.soderhielm@gmail.com>
2025-10-25 14:45:14 +0200

    Add macro for asserting compiled exe output
    Adds a new macro `assert_compiled_stdout_eq!` which compiles the
    provided source string to a executable binary and runs it, checking that
    it's stdout matches the provided expected result.

--- src/lang/test/mod.rs
+++ src/lang/test/mod.rs
@@ -1,10 +1,52 @@
+use std::{fs, process};
+
 use crate::{
+    cli,
     lang::{Program, instruction::Identifier, render::QbeRenderable},
     lex,
     parse::Parser,
     syntax,
 };
+use anyhow::anyhow;
 use similar_asserts;
+use tempfile::TempDir;
+
+/// Compiles source and executes it, asserting that stdout is equal to the expected string.
+macro_rules! assert_compiled_stdout_eq {
+    ($source:expr, $expected:expr $(,)?) => {
+        let stdout = compile_and_capture_stdout($source).unwrap();
+        similar_asserts::assert_eq!(stdout, $expected);
+    };
+}
+
+fn compile_and_capture_stdout(source: &str) -> anyhow::Result<String> {
+    let mut document = lex::Document::from_str(source);
+
+    let ast = syntax::File::parse_from(&mut document)
+        .map_err(|error| anyhow!("{}", error))?
+        .unwrap();
+
+    let program = Program::compile(&ast, document).map_err(|error| anyhow!("{}", error))?;
+
+    let directory = TempDir::new()?;
+
+    let exe_path = directory.path().join("exe");
+    let exe = fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(&exe_path)?;
+
+    cli::compile(&program, exe.into())?;
+
+    let output = process::Command::new(&exe_path)
+        .stdin(process::Stdio::null())
+        .stdout(process::Stdio::piped())
+        .output()?;
+
+    String::from_utf8(output.stdout)
+        .map_err(|error| anyhow!("Program didn't output utf8: {}", error))
+}
 
 #[test]
 fn compile_simple_program() {
@@ -30,5 +72,21 @@ fn identifier_from_symbol() {
     assert_eq!(
         Identifier::from_symbol(&symbol),
         Identifier::new("hello_u2d_hi__emoji_u1f514_"),
+    );
+}
+
+#[test]
+fn program_output_string() {
+    assert_compiled_stdout_eq!("(printf \"%s\" \"hello åäö 🔔\")", "hello åäö 🔔",);
+}
+
+#[test]
+fn program_output_function_literal() {
+    assert_compiled_stdout_eq!(
+        "\
+        (defun fun () 6)\n\
+        (printf \"%d\" (fun))\n\
+        ",
+        "6",
     );
 }
